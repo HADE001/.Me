6900e8004a7925b0241aea80136c685dcd60d871
HADE001 <kuy508138@gmail.com>
2022-10-25 14:14:51 +0700

    me_v.2

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,11 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello chaiwat</h1>
+      <a href="https://github.com/HADE001" target="_blank" rel="noopener noreferrer">      <img src="https://raw.githubusercontent.com/Arikato111/Arikato111/main/icons/github-original.svg" alt="icon github" className='logo'/></a>
+      <a href="https://www.facebook.com/profile.php?id=100019345741626" target="_blank" rel="noopener noreferrer"><img src="https://raw.githubusercontent.com/Arikato111/Arikato111/main/icons/facebook-original.svg" alt="" className='logo'/></a>
+
+
     </div>
   )
 }
